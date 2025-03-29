122f431a4e003e06a19680ee988cc372216d65b0
DavidRodriguezCardona <davidrocar1311@gmail.com>
2025-03-29 16:49:04 -0500

    DOM

---
+++ backend-naval-battle/naval.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("generarBoton");
+    const tamañoCasillas = document.getElementById("tamañoCasillas");
+    const tablero = document.getElementById("tablero")
+});
