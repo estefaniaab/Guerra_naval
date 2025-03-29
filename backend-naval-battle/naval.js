f4aae1f6af455c0c825cc4bbabe54e9624331c9f
DavidRodriguezCardona <davidrocar1311@gmail.com>
2025-03-29 16:53:59 -0500

    funcion del boton

--- backend-naval-battle/naval.js
+++ backend-naval-battle/naval.js
@@ -2,4 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("generarBoton");
     const tamañoCasillas = document.getElementById("tamañoCasillas");
     const tablero = document.getElementById("tablero")
+
+    button.addEventListener("click", () => {
+        const tamaño = parseInt(tamañoCasillas.value);
+        hacerTablero(tamaño);
+    }); 
 });
