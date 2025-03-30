5319a81f5e6d829c35c058ce72c3d4046b8dab93
DavidRodriguezCardona <davidrocar1311@gmail.com>
2025-03-29 21:37:34 -0500

    documentacion naval

--- frontend-naval-battle/controllers/naval.js
+++ frontend-naval-battle/controllers/naval.js
@@ -1,8 +1,9 @@
+//se obtienen los diferentes elementos
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("generarBoton");
     const tamañoCasillas = document.getElementById("tamañoCasillas");
     const tablero = document.getElementById("tablero")
-
+    //hacemos que el boton tenga una funcion para agregar el tamaño deseado y traerlo como numero
     button.addEventListener("click", () => {
         const tamaño = parseInt(tamañoCasillas.value);
         hacerTablero(tamaño);
@@ -11,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }); 
 });
 
+//limpiamos el tablero y agregamos las casillas de acuerdo al tamaño escogido
 function hacerTablero(tamaño) {
     const tablero = document.getElementById("tablero");
     tablero.innerHTML="";
@@ -22,7 +24,7 @@ function hacerTablero(tamaño) {
         tablero.appendChild(casilla);
     }
 }
-
+//creamos una lista de acuerdo al tamaño del tablero y la llenamos con agua osea "a"
 function crearArray(tamaño) {
     let arrayList = [];
     for (let i = 0; i < tamaño; i++) {
