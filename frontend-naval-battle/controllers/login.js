1545d6080b1d9c18541b3fbf885e7b5ab1867e2d
Redo08 <juandavidreyesortiz8@gmail.com>
2025-03-31 20:25:10 -0500

    Clima

--- frontend-naval-battle/controllers/login.js
+++ frontend-naval-battle/controllers/login.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let countries = document.getElementById("paises") // Accedemos a la componente del html
     let cambioPagina = document.getElementById("cambioPagina"); //Accedemos al id de cambio de pagina
     let cambioRanking = document.getElementById("cambioRanking")
+    const api_key = "6135afe3ddf5a712f655ec67d6bc4563"
 
     function listarPaises() {
         fetch("http://127.0.0.1:5000/countries" , {  // Se carga desde el backend
@@ -19,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 countries.innerHTML += selector
             })
         })
-        .catch(error => console.error(  "Error al obtener los paises: ", error))
+        .catch(error => console.error("Error al obtener los paises: ", error))
     }
 
     function changePage() {
@@ -34,9 +35,51 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    /**
+     * 
+     */
+    function getPosition() {
+        navigator.geolocation.getCurrentPosition(success => {
+            // Sacamos latitud y longitud
+            let latitude = success.coords.latitude
+            let longitude = success.coords.longitude
+            // Hacemos la petición
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                // Temperatura convertida a celsius
+                const tempCelsius = (data.main.temp -273.15).toFixed(1) // Kelvin -> Celsius
+                console.log(`Temperatura: ${tempCelsius}`)
+
+                // Clima con icono
+                const climaDesc = data.weather[0].description // "cielo despejado"
+                const iconCode = data.weather[0].icon        // "01d"
+                const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png` // URL del icono
+
+                //Humedad y viento
+                console.log(`Humedad: ${data.main.humidity}%`)
+                console.log(`Viento: ${data.wind.speed} m/s`)
+                console.log(`Clima: ${climaDesc}`, iconUrl)
+
+                // Ubicación y hora
+                console.log(`Ciudad: ${data.name}, ${data.sys.country}`)
+                const sunriseTime = new Date(data.sys.sunrise * 1000).toLocaleTimeString()
+                console.log(`Amanece a las: ${sunriseTime}`)
+
+            })
+            .catch(error => console.error("Error al sacar la información del clima: ", error));
+        }, failure => {
+            console.log("Error, ya no podremos sacar la información del clima");
+        })
+    }
+
     //Llamado a la función
     listarPaises()
 
     // HACER VERIFICACIÓN DE QUE SI MANDO LA INFORMACIÓN PARA DEJARLO PASAR DE PAGINA
     changePage()
+
+    // Clima
+    getPosition()
 })
