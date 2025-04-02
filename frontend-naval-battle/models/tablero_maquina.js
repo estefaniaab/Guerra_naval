7becf0e04b6f1d74003a79199ac5c1099bf64a84
DavidRodriguezCardona <davidrocar1311@gmail.com>
2025-04-02 15:05:13 -0500

    funcion para guardar el json del tablero maquina

--- frontend-naval-battle/models/tablero_maquina.js
+++ frontend-naval-battle/models/tablero_maquina.js
@@ -1,16 +1,35 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const tamañoCasillas = document.getElementById("tamañoCasillas");
-    const tamaño = parseInt(tamañoCasillas.value);
+    
+    tamañoCasillas.addEventListener("input", () =>{
+        const tamaño = parseInt(tamañoCasillas.value);
+        if (!isNaN(tamaño) && tamaño>0) {
+            let miArray=crearArray(tamaño,tamaño);
+        }
+    })
 });
-
-let miArray = crearArray(tamaño);
-     console.log(miArray);
 
 function crearArray(filas, columnas) {
     let arrayList = [];
     for (let i = 0; i < filas; i++) {
-        let subArray = new Array(columnas).fill("a");
+        let subArray = [];
+        for (let j = 0; j < columnas; j++) {
+            subArray.push("a")
+        }
         arrayList.push(subArray);
     }
     return arrayList;
-    }
+}
+
+function guardarJSON(contenido) {
+    fetch("http://127.0.0.1:5000/guardar-json", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(contenido)
+    })
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch(error => console.error("Error:", error));
+}
